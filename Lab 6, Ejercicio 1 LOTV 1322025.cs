5a0ba8e629616927a14f82f887314cb7c42703ed
luisoscar-tech <luisoscartv@gmail.com>
2026-03-03 22:48:46 -0600

    Create Lab 6, Ejercicio 1 LOTV 1322025.cs

---
+++ Lab 6, Ejercicio 1 LOTV 1322025.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=================================================");
+        Console.WriteLine("                  Ejercicio 1");
+        Console.WriteLine("=================================================");
+        Console.WriteLine("========== Clasificación de Vehículos ==========");
+        Console.WriteLine("1: Bicicleta");
+        Console.WriteLine("2: Motocicleta");
+        Console.WriteLine("3: Auto");
+        Console.WriteLine("4: Camión");
+        Console.WriteLine("5: Autobús");
+        Console.Write("Ingrese el número del vehículo: ");
+
+        int vehiculo = int.Parse(Console.ReadLine());
+
+        if (vehiculo == 1)
+        {
+            Console.WriteLine("Bicicleta → No motorizado");
+        }
+        else if (vehiculo == 2)
+        {
+            Console.WriteLine("Motocicleta → Ligero");
+        }
+        else if (vehiculo == 3)
+        {
+            Console.WriteLine("Auto → Mediano");
+        }
+        else if (vehiculo == 4)
+        {
+            Console.WriteLine("Camión → Pesado");
+        }
+        else if (vehiculo == 5)
+        {
+            Console.WriteLine("Autobús → Transporte público");
+        }
+        else
+        {
+            Console.WriteLine("ERROR: Número fuera del rango permitido.");
+        }
+
+        Console.WriteLine("\n---------------------------------------\n");
+
+    }
+}
