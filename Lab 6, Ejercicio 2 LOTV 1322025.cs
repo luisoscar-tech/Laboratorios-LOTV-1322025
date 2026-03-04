3d208c771a8d838cec3d5bbce1c323ce8636f891
luisoscar-tech <luisoscartv@gmail.com>
2026-03-03 22:59:52 -0600

    Create Lab 6, Ejercicio 2 LOTV 1322025.cs

---
+++ Lab 6, Ejercicio 2 LOTV 1322025.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("=================================================");
+        Console.WriteLine("                  Ejercicio 2");
+        Console.WriteLine("=================================================");
+
+        Console.Write("Ingrese su límite actual: ");
+        double limite = double.Parse(Console.ReadLine());
+
+        Console.Write("Ingrese el tipo de tarjeta: ");
+        int tipoTarjeta = int.Parse(Console.ReadLine());
+
+        double porcentaje;
+
+        if (tipoTarjeta == 1)
+        {
+            porcentaje = 0.25;
+        }
+        else if (tipoTarjeta == 2)
+        {
+            porcentaje = 0.35;
+        }
+        else if (tipoTarjeta == 3)
+        {
+            porcentaje = 0.40;
+        }
+        else
+        {
+            porcentaje = 0.50;
+        }
+
+        double aumento = limite * porcentaje;
+        double nuevoLimite = limite + aumento;
+
+        Console.WriteLine("Aumento aplicado: " + (porcentaje * 100) + "%");
+        Console.WriteLine("Nuevo límite de crédito: Q" + nuevoLimite);
+
+        Console.WriteLine("\n---------------------------------------\n");
+
+    }
+}
